835b788bf4608213e9353e8d897a223d9f2bbc75
Nam Tran <tranngocnam97@yahoo.com>
2017-10-28 00:59:25 -0400

    Working queue-based data

--- serial_and_milk/serial_and_milk.py
+++ serial_and_milk/serial_and_milk.py
@@ -5,14 +5,14 @@ import serial                # For reading in a serial device
 import pynmea2               # For reading NMEA 0183 sentences
 import multiprocessing       # For multiprocessing in Python
 
-#com_queue = multiprocessing.Queue()
+com_queue = multiprocessing.Queue()
 
 def print_nmea(telemetry):
     print "Latitude: {}".format(telemetry.latitude)
     print "Longitude: {}".format(telemetry.longitude)
     print "Altitude: {}\n".format(telemetry.altitude)
 
-def read_com_port(com_port):
+def read_com_port(com_port, com_queue):
     with serial.Serial(com_port, baudrate = 9600, timeout = 0.5) as ser:
 
         # Flushing any prior content within serial buffers
@@ -27,28 +27,37 @@ def read_com_port(com_port):
                 raw_sentence = ser.readline().decode('ascii', errors = 'replace') # Reads serial data
         
                 telemetry = pynmea2.parse(raw_sentence) # Parses data
-                print_nmea(telemetry)
+
+                while not com_queue.empty():
+                    com_queue.get_nowait()
                 
-                sleep(0.0)                              # To avoid reading too fast
+                com_queue.put(telemetry)
             except Exception as e:
                 print e
                 ser.close()                # Closes the serial port
+
+def read_from_queue():
+    return com_queue.get()
 
 if __name__ == '__main__':
     com_port = raw_input("Enter a COM port to listen to: ")
     
     print "Initialising Serial and Milk..."         # Prints out raw values
 
-    p = multiprocessing.Process(target=read_com_port, args=(com_port,))
+    p = multiprocessing.Process(target=read_com_port, args=(com_port,com_queue,))
     p.start()
 
     while True:
         try:
-            print "GUI would go here"
-            read_from_queue()
+            telemetry = read_from_queue()
+            print_nmea(telemetry)
             sleep(2)
         except Exception as e:
             print e
             ser.close()
+
+            # Handling closing multiprocessing stuff
+            com_queue.close()
+            com_queue.join_thread()
             p.join()
                 
